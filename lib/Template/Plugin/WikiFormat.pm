75e29748a9c5b01761b763586dd6734c8d673f72
Barbie <barbie@missbarbell.co.uk>
2015-01-06 08:15:51 +0000

    upped version

--- lib/Template/Plugin/WikiFormat.pm
+++ lib/Template/Plugin/WikiFormat.pm
@@ -2,7 +2,7 @@ package Template::Plugin::WikiFormat;
 use strict;
 use warnings;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 #----------------------------------------------------------------------------
 
